98b555d52b5b7c7c3b10d451af9d49332ae089ac
Anthony Dourish <adourish@reisystems.com>
2022-12-15 19:25:51 -0500

    added pins

--- main.py
+++ main.py
@@ -5,9 +5,19 @@ from machine import Pin
 from machine import ADC
 from machine import UART
 import neopixel
+from collections import namedtuple
+from collections import OrderedDict
+
 
 #globals
-externalLED = Pin(0, Pin.OUT)
+d = OrderedDict()
+
+d["pin0"] = Pin(0, Pin.OUT)
+d["pin1"] = Pin(1, Pin.OUT)
+d["pin10"] = Pin(10, Pin.OUT)
+d["pin11"] = Pin(11, Pin.OUT)
+d["pin12"] = Pin(12, Pin.OUT)
+
 internalLED = Pin("LED", Pin.OUT)
 np = neopixel.NeoPixel(machine.Pin(4), 8)
 uart1 = UART(1, baudrate=9600)
@@ -15,7 +25,6 @@ uart1.init(9600, bits=8, parity=None, stop=1) # init with given parameters
 t = Timer()
 
 def toggleLEDs():
-    externalLED.toggle()
     internalLED.toggle()
 
 def writeUART(message):
@@ -24,5 +33,12 @@ def writeUART(message):
 def mainLoop(timer):
     writeUART("Test UART")
     toggleLEDs()
+    loopLEDs()
     
+def loopLEDs():
+    for key, val in d.items():
+        val.toggle()
+        print("toggle:" + key)
+    
+
 t.init(freq=5000, mode=Timer.PERIODIC, callback=mainLoop)

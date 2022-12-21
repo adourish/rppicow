7c87123eee4912bd15b3d4b971f043711f4b1e35
Anth <sol9001@gmail.com>
2022-12-21 16:26:05 -0500

    bug fixes

--- mqttService.py
+++ mqttService.py
@@ -3,16 +3,12 @@ from umqttsimple  import MQTTClient
 import time
 import machine
 
-broker = settingsService.get('broker')
-sub_topic = settingsService.get('subtopic')
-pub_topic = settingsService.get('pubtopic')
-#client_id = ubinascii.hexlify(machine.unique_id())
-#client_id = mac
-client_id = settingsService.get('client_id')
+
 
 
 def init():
-    connect_and_subscribe()
+  client = connect_and_subscribe()
+
 
 def sub_cb():
     print("sub_cb")
@@ -32,7 +28,12 @@ def sub_cb(topic, msg):
 
 
 def connect_and_subscribe():
-  global client_id, mqtt_server, topic_sub
+  broker = settingsService.get('broker')
+  sub_topic = settingsService.get('subtopic')
+  pub_topic = settingsService.get('pubtopic')
+  #client_id = ubinascii.hexlify(machine.unique_id())
+  #client_id = mac
+  client_id = settingsService.get('client_id')
   client = MQTTClient(client_id, broker)
   client.set_callback(sub_cb)
   client.connect()
@@ -43,20 +44,4 @@ def connect_and_subscribe():
 def restart_and_reconnect():
   print('Failed to connect to MQTT broker. Reconnecting...')
   time.sleep(10)
-  machine.reset()
-
-try:
-  client = connect_and_subscribe()
-except OSError as e:
-  restart_and_reconnect()
-
-while True:
-  try:
-    client.check_msg()
-    if (time.time() - last_message) > message_interval:
-      pub_msg = b'Hello #%d' % counter
-      client.publish(pub_topic, pub_msg)
-      last_message = time.time()
-      counter += 1
-  except OSError as e:
-    restart_and_reconnect()
+  #machine.reset()
